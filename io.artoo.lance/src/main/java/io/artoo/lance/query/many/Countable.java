5a47f6fbd6577e6df0d338d0d251082483647add
lucaguada <luca.guadagnini@gmail.com>
2020-07-01 15:54:47 +0200

    removed peek in countable operations

--- io.artoo.lance/src/main/java/io/artoo/lance/query/many/Countable.java
+++ io.artoo.lance/src/main/java/io/artoo/lance/query/many/Countable.java
@@ -1,7 +1,6 @@
 package io.artoo.lance.query.many;
 
 import io.artoo.lance.query.cursor.Cursor;
-import io.artoo.lance.func.Cons;
 import io.artoo.lance.func.Pred;
 import io.artoo.lance.query.One;
 import io.artoo.lance.query.Queryable;
@@ -15,19 +14,17 @@ interface Countable<T> extends Queryable<T> {
   }
 
   default One<Integer> count(final Pred.Uni<? super T> where) {
-    return new Count<>(this, it -> {}, where);
+    return new Count<>(this, where);
   }
 }
 
 final class Count<T> implements One<Integer> {
   private final Queryable<T> queryable;
-  private final Cons.Uni<? super T> peek;
   private final Pred.Uni<? super T> where;
 
-  Count(final Queryable<T> queryable, final Cons.Uni<? super T> peek, final Pred.Uni<? super T> where) {
-    assert queryable != null && peek != null && where != null;
+  Count(final Queryable<T> queryable, final Pred.Uni<? super T> where) {
+    assert queryable != null && where != null;
     this.queryable = queryable;
-    this.peek = peek;
     this.where = where;
   }
 

7ddc79fd139501a4829e5cbb3c8ebd50d24e6321
lucaguada <luca.guadagnini@gmail.com>
2020-07-01 16:33:20 +0200

    fixed tests for insertable operations

--- io.artoo.lance/src/main/java/io/artoo/lance/query/many/Insertable.java
+++ io.artoo.lance/src/main/java/io/artoo/lance/query/many/Insertable.java
@@ -1,51 +1,42 @@
 package io.artoo.lance.query.many;
 
 import io.artoo.lance.query.cursor.Cursor;
-import io.artoo.lance.func.Cons;
 import io.artoo.lance.query.Many;
 import io.artoo.lance.query.Queryable;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
 
 import static io.artoo.lance.type.Nullability.nonNullable;
 
 public interface Insertable<T> extends Queryable<T> {
   @SuppressWarnings("unchecked")
-  default Many<T> insert(final T... values) {
-    return new Insert<>(this, (i, it) -> {}, Many.from(nonNullable(values, "values")));
+  default Many<T> insert(final T... elements) {
+    return new Insert<>(this, Many.from(nonNullable(elements, "elements")));
   }
 
   default <Q extends Queryable<T>> Many<T> insert(final Q queryable) {
-    return new Insert<>(this, (i, it) -> {}, nonNullable(queryable, "queryable"));
+    return new Insert<>(this, nonNullable(queryable, "queryable"));
   }
 }
 
-// TODO: where and select are missing
 final class Insert<T, Q extends Queryable<T>> implements Many<T> {
   private final Queryable<T> queryable;
-  private final Cons.Bi<? super Integer, ? super T> peek;
-  private final Q values;
+  private final Q elements;
 
-  Insert(final Queryable<T> queryable, final Cons.Bi<? super Integer, ? super T> peek, final Q values) {
+  Insert(final Queryable<T> queryable, final Q elements) {
+    assert queryable != null && elements != null;
     this.queryable = queryable;
-    this.peek = peek;
-    this.values = values;
+    this.elements = elements;
   }
 
   @NotNull
   @Override
   public final Cursor<T> cursor() {
-    final var array = new ArrayList<T>();
-    var index = 0;
-    for (var cursor = queryable.iterator(); cursor.hasNext(); index++) {
-      var it = cursor.next();
-      peek.accept(index, it);
-      if (it != null)
-        array.add(it);
+    final var cursor = queryable.cursor();
+
+    for (final var element : elements) {
+      cursor.append(element);
     }
-    for (final var value : values)
-      array.add(value);
-    return Cursor.from(array.iterator());
+
+    return cursor;
   }
 }

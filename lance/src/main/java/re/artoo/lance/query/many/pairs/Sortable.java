405f361204033c8670508c4bce66251e4fc74e9c
lucaguada <luca.guadagnini@gmail.com>
2023-01-14 15:05:05 +0100

    temporary remove of feature pairs

--- lance/src/main/java/re/artoo/lance/query/many/pairs/Sortable.java
+++
@@ -1,9 +0,0 @@
-package re.artoo.lance.query.many.pairs;
-
-import re.artoo.lance.Queryable;
-
-public interface Sortable<A, B> extends Queryable.OfTwo<A, B> {
-  default Ordering<A, B> order() {
-    return new Order<>(this);
-  }
-}

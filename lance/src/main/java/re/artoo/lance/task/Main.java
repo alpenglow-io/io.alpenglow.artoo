4a2b7a3d3aaecd75c411859d319b27f0c040b1d5
lucaguada <luca.guadagnini@gmail.it>
2023-08-07 19:37:51 +0200

    trying to fix the bug with completable-future

--- lance/src/main/java/re/artoo/lance/task/Main.java
+++ lance/src/main/java/re/artoo/lance/task/Main.java
@@ -1,17 +1,15 @@
 package re.artoo.lance.task;
 
 import static re.artoo.lance.task.Task.async;
+import static re.artoo.lance.task.Task.success;
 
 interface Main {
   static Task<Integer> fibonacci(int value) {
-    if (value == 0 || value == 1) return async(() -> value);
-    var n1 = fibonacci(value - 1);
-    var n2 = fibonacci(value - 2);
-    return async(() -> n1.await() + n2.await());
+    if (value == 0 || value == 1) return success(value);
+    return async() fibonacci(value - 1).then(fibonacci(value - 2), (f1, fib) -> fib.then(f2 -> f1 + f2));
   }
 
-  static void main(String... args) {
-    var fibonacci = fibonacci(10);
-    System.out.println(fibonacci.await());
+  static void main(String... args) throws InterruptedException {
+    System.out.println(fibonacci(1000).await());
   }
 }
--- lance/src/main/java/re/artoo/lance/task/Task.java
+++ lance/src/main/java/re/artoo/lance/task/Task.java
@@ -1,17 +1,12 @@
 package re.artoo.lance.task;
 
-import re.artoo.lance.func.InvokeException;
-import re.artoo.lance.func.TryFunction1;
-import re.artoo.lance.query.Cursor;
-import re.artoo.lance.query.One;
-import re.artoo.lance.task.Tasks.Promise;
+import re.artoo.lance.func.*;
 
-import java.util.Deque;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.*;
+import java.util.function.Function;
 
-import static java.util.Objects.requireNonNull;
 import static re.artoo.lance.task.Tasks.Pool;
 
 enum Tasks {
@@ -19,60 +14,208 @@ enum Tasks {
 
   final Map<UUID, Callable<?>> operations = new ConcurrentHashMap<>();
   final Map<UUID, Future<?>> results = new ConcurrentHashMap<>();
-
-  record Promise(UUID id, Callable<?> operation) {}
 }
 
-sealed interface Task<T> {
-  static <T> Task<T> async(Callable<T> operation) {
-    return new Async<>(operation);
+interface Awaitable extends Runnable {
+  @Override
+  default void run() {
+    try {
+      if (!Pool.operations.isEmpty()) {
+        System.out.println("Operations: " + Pool.operations.size());
+        try (final var tasks = Executors.newVirtualThreadPerTaskExecutor()) {
+          for (Map.Entry<UUID, Callable<?>> entry : Pool.operations.entrySet()) {
+            Pool.results.putIfAbsent(entry.getKey(), tasks.submit(entry.getValue()));
+            Pool.operations.remove(entry.getKey());
+          }
+
+          if (!tasks.awaitTermination(2, TimeUnit.SECONDS)) tasks.shutdown();
+        }
+      }
+    } catch (InterruptedException | IllegalStateException e) {
+      throw new InvokeException(e);
+    }
   }
-  static <T> Task<T> success(T value) { return new Success<>(value); }
+}
+
+@SuppressWarnings("unchecked")
+public sealed interface Task<T> {
+  final class Exception extends RuntimeException {
+    public Exception(Throwable throwable) {
+      super(throwable);
+    }
+
+    public Exception(String cause) {
+      super(cause);
+    }
+  }
+
+  static <T> Task<T> async(Callable<T> operation) {
+    return new Running<>(operation);
+  }
+
+  static <T> Task<T> nothing() {
+    return (Task<T>) Nothing.Default;
+  }
+
+  static <T> Task<T> success(T value) {
+    return new Success<>(value);
+  }
+
+  static <T> Task<T> failure(Throwable throwable) {
+    return new Failure<>(throwable);
+  }
+
+  default <R> Task<R> then(TryFunction1<? super T, ? extends R> operation) {
+    throw new Task.Exception("Can't retrieve result for modifiable then operation, result is nothing");
+  }
+
+  @FunctionalInterface
+  interface TaskFunction<T, R> extends TryFunction1<T, R> {
+  }
+
+  default <R> Task<R> then(TaskFunction<? super T, ? extends Task<R>> operation) {
+    throw new Task.Exception("Can't retrieve result for composable then operation, result is nothing");
+  }
+
+  default <R> Task<R> then(Task<R> task, TryFunction2<? super T, ? super Task<R>, ? extends Task<R>> operation) {
+    throw new Task.Exception("Can't retrieve result for composable then operation, result is nothing");
+  }
+
+  default Task<T> when(TryPredicate1<? super T> condition) {
+    throw new Task.Exception("Can't retrieve result for conditional then, result is nothing");
+  }
+
+  default Task<T> then(TryConsumer1<? super T> operation) {
+    throw new Task.Exception("Can't retrieve result for unmodifiable then operation, result is nothing");
+  }
 
   T await();
 
-  record Success<T>(UUID id) implements Task<T> {
-    Success(T value) {
-      this(UUID.randomUUID());
-      Pool.results.put(id, new FutureTask<>(() -> value));
-    }
+  enum Nothing implements Task<Object>, Awaitable {
+    Default;
 
-    @SuppressWarnings("unchecked")
-    public T await() {
-      try {
-        return (T) Pool.results.get(id).get();
-      } catch (InterruptedException | ExecutionException e) {
-        throw new InvokeException(e);
-      }
+    @Override
+    public Object await() {
+      run();
+      throw new Task.Exception("Can't retrieve result, result is nothing");
     }
   }
 
-  record Async<T>(UUID id) implements Task<T> {
-    Async(Callable<T> operation) {
-      this(UUID.randomUUID());
-      Pool.operations.put(id, operation);
+  @SuppressWarnings("unchecked")
+  record Success<T>(T result) implements Task<T>, Awaitable {
+    @Override
+    public <R> Task<R> then(TryFunction1<? super T, ? extends R> operation) {
+      return new Success<>(operation.apply(result));
     }
 
-    @SuppressWarnings("unchecked")
+    @Override
+    public <R> Task<R> then(Task<R> task, TryFunction2<? super T, ? super Task<R>, ? extends Task<R>> operation) {
+      return operation.apply(result, task);
+    }
+
+    @Override
+    public Task<T> when(TryPredicate1<? super T> condition) {
+      return condition.test(result) ? this : (Task<T>) Nothing.Default;
+    }
+
+    @Override
+    public Task<T> then(TryConsumer1<? super T> operation) {
+      operation.accept(result);
+      return this;
+    }
+
+    @Override
+    public T await() {
+      run();
+      return result;
+    }
+  }
+
+  record Failure<T>(Throwable throwable) implements Task<T>, Awaitable {
+    @Override
+    public <R> Task<R> then(TryFunction1<? super T, ? extends R> operation) {
+      throw new Task.Exception(throwable);
+    }
+
+    @Override
+    public <R> Task<R> then(Task<R> task, TryFunction2<? super T, ? super Task<R>, ? extends Task<R>> operation) {
+      throw new Task.Exception(throwable);
+    }
+
+    @Override
+    public Task<T> when(TryPredicate1<? super T> condition) {
+      throw new Task.Exception(throwable);
+    }
+
+    @Override
+    public Task<T> then(TryConsumer1<? super T> operation) {
+      throw new Task.Exception(throwable);
+    }
+
+    @Override
+    public T await() {
+      run();
+      throw new Task.Exception(throwable);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  record Running<T>(CompletableFuture<T> future) implements Task<T>, Awaitable {
+    private static final Object NORESULT = new Object();
+
+    Running(Callable<T> operation) {
+      this(CompletableFuture.supplyAsync((TrySupplier1<T>) operation::call, Executors.newVirtualThreadPerTaskExecutor()));
+    }
+
+    @Override
+    public <R> Task<R> then(TryFunction1<? super T, ? extends R> operation) {
+      return new Running<>(future.thenApplyAsync(operation));
+    }
+
+    @Override
+    public <R> Task<R> then(TaskFunction<? super T, ? extends Task<R>> operation) {
+      try {
+        return switch (future.thenComposeAsync(staging(operation)).get()) {
+          case Throwable throwable -> Task.failure(throwable);
+          case Object object when object.equals(NORESULT) -> Task.nothing();
+          case Object object -> Task.success((R) object);
+        };
+      } catch (InterruptedException | ExecutionException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    private static <T, R> Function<T, CompletableFuture<Object>> staging(TaskFunction<? super T, ? extends Task<R>> operation) {
+      return it -> switch (operation.apply(it)) {
+        case Success<R> success -> CompletableFuture.completedFuture(success.result);
+        case Failure<R> failure -> CompletableFuture.failedFuture(failure.throwable);
+        case Nothing nothing -> CompletableFuture.completedFuture(NORESULT);
+        case Running<R> running -> running.future.thenApplyAsync(p -> p);
+      };
+    }
+
+    @Override
+    public <R> Task<R> then(Task<R> task, TryFunction2<? super T, ? super Task<R>, ? extends Task<R>> operation) {
+      return operation.apply(await(), task);
+    }
+
+    @Override
+    public Task<T> when(TryPredicate1<? super T> condition) {
+      return then(it -> condition.test(it) ? Task.success(it) : Task.nothing());
+    }
+
+    @Override
+    public Task<T> then(TryConsumer1<? super T> operation) {
+      return then((TryFunction1<? super T, ? extends T>) it -> {
+        operation.accept(it);
+        return it;
+      });
+    }
+
     public T await() {
       try {
-        if (!Pool.operations.isEmpty()) {
-          System.out.printf("Dimensione delle promises: %d%n", Pool.operations.size());
-          try (final var tasks = Executors.newVirtualThreadPerTaskExecutor()) {
-            for (Map.Entry<UUID, Callable<?>> entry : Pool.operations.entrySet()) {
-              Pool.results.putIfAbsent(entry.getKey(), tasks.submit(entry.getValue()));
-              Pool.operations.remove(entry.getKey());
-            }
-          }
-        }
-        var resulted = (T) Pool.results.get(id).get(1, TimeUnit.SECONDS);
-        Pool.operations.remove(id);
-        return resulted;
-      } catch (IllegalStateException e) {
-        e.printStackTrace();
-        throw new InvokeException(e);
-      } catch (ExecutionException | InterruptedException | TimeoutException e) {
-        e.printStackTrace();
+        return future.get();
+      } catch (InterruptedException | ExecutionException e) {
         throw new RuntimeException(e);
       }
     }
